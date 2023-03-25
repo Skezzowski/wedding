2356249761ac2df13362a4a01988ec3104c784f1
Skezzowski <sipblai@gmail.com>
2023-03-25 12:15:23 +0100

    header and logo fixes

--- src/common/Button/styles.ts
+++ src/common/Button/styles.ts
@@ -7,13 +7,12 @@ export const StyledButton = styled("button")<any>`
   font-weight: 700;
   width: 100%;
   border: 1px solid #edf3f5;
-  border-radius: 4px;
+  border-radius: 10px;
   padding: 13px 0;
   cursor: pointer;
   margin-top: 0.625rem;
   max-width: 180px;
   transition: all 0.3s ease-in-out;
-  box-shadow: 0 16px 30px rgb(23 31 114 / 20%);
 
   &:hover,
   &:active,
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -58,7 +58,7 @@ const Header = ({ t }: any) => {
       <Container>
         <Row justify="space-between">
           <LogoContainer to="/" aria-label="homepage">
-            <SvgIcon src="logo.svg" width="101px" height="64px" />
+            <SvgIcon src="logo.svg" width="85px" height="55px" />
           </LogoContainer>
           <NotHidden>
             <MenuItem />
