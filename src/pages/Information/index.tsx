24215c59808b813a7665e192f39d6e56bbfccb0c
Skezzowski <sipblai@gmail.com>
2023-03-25 13:07:40 +0100

    Add more information and slide

--- src/pages/Information/index.tsx
+++ src/pages/Information/index.tsx
@@ -1,4 +1,5 @@
 import { lazy } from "react";
+import { Slide } from "react-awesome-reveal";
 import styled from "styled-components";
 
 const Container = lazy(() => import("../../common/Container"));
@@ -25,50 +26,74 @@ export const MiddleBlockSection = styled("section")`
 `;
 
 export const TextParagraph = styled("div")`
-  padding: 2rem;
+  padding: 1rem;
 `;
 
 const Information = () => {
   return (
     <MainContentContainer>
-      <img src="/img/izlandkiss7.png" width={"100%"} alt="" /> 
+      <img src="/img/izlandkiss7.png" width={"100%"} alt="" />
       <Container>
-        <TextParagraph>
-          <h6>
-            Összeházasodunk!
-          </h6>
-          <p>
-            Kedves Családunk és Barátaink, ezt a weboldalt azért hoztuk létre, hogy minden fontos információt könnyedén megtaláljatok.
-            Szeretettel várunk benneteket a <b>Lósi Majorban 2023.09.23-án</b>
-            <br/>
-            <br/> 
-            Kérünk titeket, hogy minél előbb jelezzetek vissza a jobb felső sarokban található menüpont segítségével!
-          </p>
-        </TextParagraph>
-        <TextParagraph>
-          <h6>
-            Helyszín
-          </h6>
-          <p>
-            Az esküvő helyszíne a Lósi Major. A lenti térképen láthatjátok merre található. parkolás stb....
-            <br/>
-            <br/>
-            Sajnos szállást nem tudunk biztosítani, de egy részletes listát találhattok a "Szállások" menüpont alatt, ezeket mind teljes szívvel ajánljuk.
-            Mivel a telek nagy része a miénk erre a kis időre, így a válalkozó szellemüek akár sátorral/lakókocsival is érkezhetnek!
-          </p>
-          <div style={{width: '100%', height: '32rem', flex: 1, flexGrow: 2, minWidth: '60%'}}>
-                <iframe title="Szállás Térkép" src="https://www.google.com/maps/d/u/0/embed?mid=10yBO2wGA57re8MVqKxJv2thTHq-oVFU&ehbc=2E312F" width="100%" height="100%"></iframe>
-              </div>
+        <Slide direction="left">
+          <TextParagraph>
+            <h6>
+              Összeházasodunk!
+            </h6>
+            <p>
+              Kedves Családunk és Barátaink, ezt a weboldalt azért hoztuk létre, hogy minden fontos információt könnyedén megtaláljatok.
+            </p>
+            <p>
+              Szeretettel várunk benneteket a <b>Lósi Majorban 2023.09.23-án</b>
+            </p>
+            <p>
+              Kérünk titeket, hogy minél előbb jelezzetek vissza a jobb felső sarokban található menüpont segítségével!
+            </p>
+          </TextParagraph>
+        </Slide>
+        <Slide direction="left">
+          <TextParagraph>
+            <h6>
+              Helyszín
+            </h6>
+            <p>
+              Az esküvő helyszíne a <b>Lósi Major</b>.<br />
+              <b>Cím</b>: 2621 Verőce, Lósi völgy út 1.<br />
+              <b>Koordináták</b>: É47,82867   K19,05899<br />
+            </p>
+            <p>
+              Sajnos szállást nem tudunk biztosítani, de egy részletes listát találhattok a "Szállások" menüpont alatt, ezeket mind teljes szívvel ajánljuk.
+            </p>
+            <p>
+            Mivel a telek nagy része a miénk erre a kis időre, így a válalkozó szellemüek akár <b>sátorral </b> vagy <b>lakókocsival</b> is érkezhetnek!
+            </p>
+            <div style={{ width: '100%', height: '32rem', flex: 1, flexGrow: 2, minWidth: '60%' }}>
+              <iframe title="Szállás Térkép" src="https://www.google.com/maps/d/u/0/embed?mid=10yBO2wGA57re8MVqKxJv2thTHq-oVFU&ehbc=2E312F" width="100%" height="100%"></iframe>
+            </div>
+          </TextParagraph>
+        </Slide>
 
-        </TextParagraph>
-        <TextParagraph>
-          <h6>
-            Ajándékon gondolkozol?
-          </h6>
-          <p>
-            Ne tedd! Egy borítékban könnyen elrejtheted. 
-          </p>
-        </TextParagraph>
+        <Slide direction="left">
+          <TextParagraph>
+            <h6>
+              Fontos tudni
+            </h6>
+            <p>
+            • Ajándékot csak olyan formában hozzatok, hogy malacperselyünk is meg tudja emészteni!
+            </p>
+            <p>
+            • A mai gazdasági helyzetre tekintettel, a menyecsketánc elmarad. Természetesen attól még a menyecske megtáncoltatható. Megértéseket köszönjük!
+            </p>
+            <p>
+            • Ló és kecskeharapásért felelőséget nem vállalunk! A lámáért igen.
+            </p>
+            <p>
+            • A Mennyaszonyrablást a törvény bünteti, de attól még lehet.
+            </p>
+            <p>
+            • Atuókkal az autó-karámba parkoljatok, hogy nehogy megszökjenek.
+            </p>
+          </TextParagraph>
+        </Slide>
       </Container>
     </MainContentContainer>
   );
